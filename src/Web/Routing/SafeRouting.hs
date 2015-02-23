f6eed6f1e80f580e0d74bd75f587da3982b63fd8
Tim Baumann <tim@timbaumann.info>
2015-02-23 19:20:13 +0100

    Add subcomponents to SafeRouting

--- src/Web/Routing/SafeRouting.hs
+++ src/Web/Routing/SafeRouting.hs
@@ -12,9 +12,7 @@ import Data.HVect
 import Web.Routing.AbstractRouter
 
 import Data.Maybe
-import Data.List (foldl')
 import Data.Monoid (Monoid (..))
-import Control.Applicative (Applicative (..), Alternative (..))
 import Data.String
 import Data.Typeable (Typeable)
 import Control.DeepSeq (NFData (..))
@@ -62,46 +60,38 @@ data Path (as :: [*]) where
 
 data PathMap x =
   PathMap
-  { pm_here :: [x]
+  { pm_subComponents :: [[T.Text] -> x]
+  , pm_here :: [x]
   , pm_staticMap :: HM.HashMap T.Text (PathMap x)
   , pm_polyMap :: PM.PolyMap PathPiece PathMap x
   }
 
 instance Functor PathMap where
-  fmap f (PathMap h s p) = PathMap (fmap f h) (fmap (fmap f) s) (fmap f p)
-
-instance Applicative PathMap where
-  pure x = PathMap [x] mempty PM.empty
-  PathMap h s p <*> y =
-    let start = PathMap mempty (fmap (<*> y) s) (PM.updateAll (\a -> fmap flip a <*> y) p)
-    in foldl' (\pm f -> pm `mappend` fmap f y) start h
-
-instance Alternative PathMap where
-  empty = emptyPathMap
-  (<|>) = mappend
+  fmap f (PathMap sc h s p) = PathMap (fmap (fmap f) sc) (fmap f h) (fmap (fmap f) s) (fmap f p)
 
 instance NFData x => NFData (PathMap x) where
-  rnf (PathMap h s p) = rnf h `seq` rnf s `seq` PM.rnfHelper rnf p
+  rnf (PathMap sc h s p) = rnf sc `seq` rnf h `seq` rnf s `seq` PM.rnfHelper rnf p
 
 emptyPathMap :: PathMap x
-emptyPathMap = PathMap mempty mempty PM.empty
+emptyPathMap = PathMap [] mempty mempty PM.empty
 
 instance Monoid (PathMap x) where
   mempty = emptyPathMap
-  mappend (PathMap h1 s1 p1) (PathMap h2 s2 p2) =
-    PathMap (h1 `mappend` h2) (HM.unionWith mappend s1 s2) (PM.unionWith mappend p1 p2)
+  mappend (PathMap sc1 h1 s1 p1) (PathMap sc2 h2 s2 p2) =
+    PathMap (sc1 `mappend` sc2) (h1 `mappend` h2)
+            (HM.unionWith mappend s1 s2) (PM.unionWith mappend p1 p2)
 
 insertPathMap' :: Path ts -> (HVect ts -> x) -> PathMap x -> PathMap x
-insertPathMap' path action (PathMap h s p) =
+insertPathMap' path action (PathMap sc h s p) =
   case path of
-    Empty -> PathMap (action HNil : h) s p
+    Empty -> PathMap sc (action HNil : h) s p
     StaticCons pathPiece path' ->
       let subPathMap = fromMaybe emptyPathMap (HM.lookup pathPiece s)
-      in PathMap h (HM.insert pathPiece (insertPathMap' path' action subPathMap) s) p
+      in PathMap sc h (HM.insert pathPiece (insertPathMap' path' action subPathMap) s) p
     VarCons path' ->
       let alterFn = Just . insertPathMap' path' (\vs v -> action (HCons v vs))
                          . fromMaybe emptyPathMap
-      in PathMap h s (PM.alter alterFn p)
+      in PathMap sc h s (PM.alter alterFn p)
 
 singleton :: Path ts -> HVectElim ts x -> PathMap x
 singleton path action = insertPathMap' path (hVectUncurry action) mempty
@@ -109,13 +99,32 @@ singleton path action = insertPathMap' path (hVectUncurry action) mempty
 insertPathMap :: RouteHandle m a -> PathMap (m a) -> PathMap (m a)
 insertPathMap (RouteHandle path action) = insertPathMap' path (hVectUncurry action)
 
+insertSubComponent' :: Path ts -> (HVect ts -> [T.Text] -> x) -> PathMap x -> PathMap x
+insertSubComponent' path comp (PathMap sc h s p) =
+  case path of
+    Empty -> PathMap (comp HNil : sc) h s p
+    StaticCons pathPiece path' ->
+      let subPathMap = fromMaybe emptyPathMap (HM.lookup pathPiece s)
+      in PathMap sc h (HM.insert pathPiece (insertSubComponent' path' comp subPathMap) s) p
+    VarCons path' ->
+      let alterFn = Just . insertSubComponent' path' (\vs ps v -> comp (HCons v vs) ps)
+                         . fromMaybe emptyPathMap
+      in PathMap sc h s (PM.alter alterFn p)
+
+insertSubComponent :: Functor m => RouteHandle m ([T.Text] -> a) -> PathMap (m a) -> PathMap (m a)
+insertSubComponent (RouteHandle path comp) =
+  insertSubComponent' path (fmap (\m ps -> fmap ($ ps) m) (hVectUncurry comp))
+
 match :: PathMap x -> [T.Text] -> [x]
-match (PathMap h _ _) [] = h
-match (PathMap _ s p) (pp:pps) =
-  let staticMatches = maybeToList (HM.lookup pp s) >>= flip match pps
-      varMatches = PM.lookupConcat (fromPathPiece pp)
-                     (\piece pathMap' -> fmap ($ piece) (match pathMap' pps)) p
-  in staticMatches ++ varMatches
+match (PathMap sc h s p) pieces =
+  map ($ pieces) sc ++
+  case pieces of
+    [] -> h
+    (pp:pps) ->
+      let staticMatches = maybeToList (HM.lookup pp s) >>= flip match pps
+          varMatches = PM.lookupConcat (fromPathPiece pp)
+                         (\piece pathMap' -> fmap ($ piece) (match pathMap' pps)) p
+      in staticMatches ++ varMatches
 
 -- | A route parameter
 var :: (Typeable a, PathPiece a) => Path (a ': '[])
--- test/Web/Routing/SafeRoutingSpec.hs
+++ test/Web/Routing/SafeRoutingSpec.hs
@@ -10,9 +10,10 @@ import Data.HVect
 import Control.Monad.Identity
 import Web.Routing.SafeRouting
 import Web.Routing.AbstractRouter
-import Data.Monoid (mconcat)
-import Control.Applicative (Applicative (..))
 import qualified Data.Text as T
+import Control.Monad.RWS.Strict
+import Data.Maybe
+import qualified Data.HashMap.Strict as HM
 
 data ReturnVar
    = IntVar Int
@@ -23,6 +24,21 @@ data ReturnVar
 
 defR :: (Monad m, m ReturnVar ~ x) => Path ts -> HVectElim ts x -> RegistryT (SafeRouter m ReturnVar) middleware Bool m ()
 defR path action = hookRoute True (SafeRouterPath path) (HVectElim' action)
+
+-- TODO: abstract this code, move into AbstractRouter
+defSubComponent :: (Monad m, Functor m, m ([T.Text] -> ReturnVar) ~ x)
+                 => Path ts
+                 -> HVectElim ts x
+                 -> RegistryT (SafeRouter m ReturnVar) middleware Bool m ()
+defSubComponent path comp =
+    do let reqType = True
+       SafeRouterPath basePath <- ask
+       modify $ \rs ->
+           rs { rs_registry =
+                    let SafeRouterReg (reg, fb) = fromMaybe emptyRegistry (HM.lookup reqType (rs_registry rs))
+                        reg' = insertSubComponent (RouteHandle (basePath </> path) comp) reg
+                    in HM.insert reqType (SafeRouterReg (reg', fb)) (rs_registry rs)
+              }
 
 spec :: Spec
 spec =
@@ -43,25 +59,11 @@ spec =
           do checkRoute "/bar/5" [IntVar 5, StrVar "5"]
              checkRoute "/bar/bingo" [StrVar "bar/bingo", StrVar "bingo"]
              checkRoute "/entry/1/audit" [IntVar 1,ListVar [IntVar 1,StrVar "audit"]]
-       it "should provide an Applicative interface" $
-          do let numbers =
-                   mconcat
-                   [ singleton var id
-                   , singleton ("forty" </> "two") (42 :: Int)
-                   ]
-                 operators =
-                   mconcat
-                   [ singleton "plus" ((+) :: Int -> Int -> Int)
-                   , singleton "mult" (*)
-                   ]
-                 routes = operators <*> numbers <*> numbers
-                 check path val = match routes (pieces path) `shouldBe` [val]
-             check "/plus/forty/two/forty/two" (42+42)
-             check "/mult/forty/two/3" (42*3)
-             check "/plus/5/89" 94
        it "should have a catch all route" $
           do checkRoute "/aslkdjk/asdaskl/aslkjd" [StrVar "aslkdjk/asdaskl/aslkjd"]
              checkRoute "/zuiasf/zuiasf" [StrVar "zuiasf/zuiasf"]
+       it "should hand over remaining path pieces to subcomponents" $
+          do checkRoute "/subcomponent/blog/foo/bar/nanana" [StrVar "blog:foo?bar?nanana"]
     where
       pieces :: T.Text -> [T.Text]
       pieces = filter (not . T.null) . T.splitOn "/"
@@ -94,4 +96,6 @@ spec =
              defR ("bar" </> "bingo") $ return (StrVar "bar/bingo")
              defR ("bar" </> var) $ (return . StrVar . T.pack)
              defR ("entry" </> var </> "audit") (return . IntVar)
+             defSubComponent ("subcomponent" </> var) $ \name ->
+               return $ \ps -> StrVar $ name <> ":" <> T.intercalate "?" ps
              hookAny True (return . StrVar . T.intercalate "/")
